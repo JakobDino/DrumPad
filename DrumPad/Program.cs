f3f176ed64d5f7644c3d051566ee9ef2994b4179
Dino <jakobhugo2005@gmail.com>
2024-01-02 13:04:36 +0100

    Single instance added

---
+++ DrumPad/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+using Windows.Media.Core;
+
+namespace DrumPad
+{
+    class Program
+    {
+        class program
+        {
+            [STAThread]
+            static async Task<int> Main(string[] args)
+            {
+                WinRT.ComWrappersSupport.InitializeComWrappers();
+                bool isRedirect = await DecideRedirection();
+                if (!isRedirect)
+                {
+                    Microsoft.UI.Xaml.Application.Start((p) =>
+                    {
+                        var context = new DispatcherQueueSynchronizationContext(
+                            DispatcherQueue.GetForCurrentThread());
+                        SynchronizationContext.SetSynchronizationContext(context);
+                        new App();
+                    });
+                }
+                return 0;
+            }
+            private static async Task<bool> DecideRedirection()
+            {
+                bool isRedirect = false;
+                AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+                ExtendedActivationKind kind = args.Kind;
+                AppInstance keyInstance = AppInstance.FindOrRegisterForKey("randomKey");
+
+                if (keyInstance.IsCurrent)
+                {
+                    keyInstance.Activated += OnActivated;
+                }
+                else
+                {
+                    isRedirect = true;
+                    await keyInstance.RedirectActivationToAsync(args);
+                }
+                return isRedirect;
+            }
+            private static void OnActivated(object sender, AppActivationArguments args)
+            {
+                ExtendedActivationKind kind = args.Kind;
+            }
+        }
+    }
+}
